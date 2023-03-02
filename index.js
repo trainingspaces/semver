5f366ed0dd82452698e82b19f52628f8a9cf2833
Yosvel Reyes <yrfonfria@gmail.com>
2023-03-02 20:06:09 -0300

    more changes v12

--- index.js
+++ index.js
@@ -3,17 +3,31 @@ const gh = require('@actions/github');
 const path = require('path');
 const fs = require('fs/promises');
 
-const readPkgJson = async (file) => {
-    const content = await fs.readFile(file);
-    return JSON.parse(content.toString());
+const getVersion = async (filePath) => {
+    const content = await fs.readFile(filePath);
+    const pkg = JSON.parse(content.toString());
+    core.setOutput('version', pkg.version);
+}
+
+const buildUri = (actor, token, uri) => {
+
+    if(!url.startsWith('https://')) throw new Error('not an https uri, can not continue');
+    core.setOutput('repoUri', uri.replace('https://', `https://${actor}:${token}@`));
 }
 
 const run = async () => {
     
     try {
-        const location = core.getInput('location');
-        const pkg = await readPkgJson(path.join(process.env.GITHUB_WORKSPACE, location));
-        core.setOutput('version', pkg.version);
+        const path = core.getInput('path');
+        const fullPath = path.join(process.env.GITHUB_WORKSPACE, path);
+        await getVersion(fullPath);
+
+        buildUri(
+            core.getInput('actor', { required: true }),
+            core.getInput('uri', { required: true }),
+            core.getInput('token', { required: true })
+        );
+
     } catch (e) {
         core.setFailed(e.message);
     }
